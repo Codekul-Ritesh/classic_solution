7548627b7fc1e50e652f3ac72c697c4c64df7177
Codekul-Ritesh <ritesh.tagunde2402@gmail.com>
2024-03-02 13:32:38 +0530

    header-footer

---
+++ src/Footer.js
@@ -0,0 +1,167 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+
+<div id="cdfooter" class="footer rev-7-footer">
+        <div class="container">
+            <div class="footer-subscribe">
+                <div class="row align-items-center">
+                    <div class="col-xxl-3 col-xl-4">
+                        <h3>Our Newsletter Now</h3>
+                    </div>
+
+                    <div class="col-xxl-4 col-xl-4 col-lg-9 col-11 col-xs-12">
+                        <form action="#" class="footer-subs-form">
+                            <input type="text" name="Subscription-form" id="footer-subscribe-input" placeholder="Your emal Address"/>
+                            <button class="subs-btn">Subscribe</button>
+                        </form>
+                    </div>
+
+                    <div class="col-xxl-5 col-xl-4">
+                        <div class="footer-socials">
+                            <h3 class="footer-title">Join us on</h3>
+                            <ul>
+                                <li>
+                                    <a href="https://instagram.com/" target="_blank">
+                                        <img src="assets/images/socials/fb.png" alt="facebook logo"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/" target="_blank">
+                                        <img src="assets/images/socials/pt.png" alt="pinterest logo"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/" target="_blank">
+                                        <img src="assets/images/socials/lk.png" alt="linkedin logo"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/" target="_blank">
+                                        <img src="assets/images/socials/tw.png" alt="twitter logo"/>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/" target="_blank">
+                                        <img src="assets/images/socials/in.png" alt="instagram logo"/>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-footer">
+            <div class="container">
+                <div class="custom-row">
+                    <div class="custom-col-2">
+                        <div class="footer-about">
+                            <div class="footer-logo">
+                                <img src="assets/images/logos/logo-light-6.png" alt="Logo"/>
+                            </div>
+                            <ul>
+                                <li>
+                                    <div class="icon">
+                                        <span><i class="flaticon-map"></i></span>
+                                    </div>
+                                    <div class="txt">
+                                        <span>4920 Trails End Road Ft United States, FL 33311</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <span><i class="flaticon-email"></i></span>
+                                    </div>
+                                    <div class="txt">
+                                        <a href="mailto:info@example.com">info@example.com</a>
+                                        <a href="mailto:test@example.com">test@example.com</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <span><i class="flaticon-telephone"></i></span>
+                                    </div>
+                                    <div class="txt">
+                                        <a href="tel:+123456679123">+123 456 679 123</a>
+                                        <a href="tel:+123456789">+123 456 789</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="custom-col-2">
+                        <div class="link-wrap">
+                            <div class="footer-link">
+                                <h3 class="footer-title">My Account</h3>
+                                <ul>
+                                    <li><a href="about.html">About Us</a></li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                    <li><a href="blog.html">Blogs</a></li>
+                                    <li><a href="#">Terms Of Use</a></li>
+                                    <li><a href="#">Privacy Policies</a></li>
+                                </ul>
+                            </div>
+                            <div class="footer-link">
+                                <h3 class="footer-title">Information</h3>
+                                <ul>
+                                    <li><a href="cart.html">My Order</a></li>
+                                    <li><a href="wishlist.html">Wishlist</a></li>
+                                    <li><a href="account.html">My Credit</a></li>
+                                    <li><a href="register.html">Login</a></li>
+                                    <li><a href="account.html">Personal Info</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="custom-col-2">
+                        <div class="link-wrap">
+                            <div class="footer-link">
+                                <h3 class="footer-title">Custom Links</h3>
+                                <ul>
+                                    <li><a href="about.html">About Us</a></li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                    <li><a href="blog.html">Blogs</a></li>
+                                    <li><a href="#">Terms Of Use</a></li>
+                                    <li><a href="#">Privacy Policies</a></li>
+                                </ul>
+                            </div>
+                            <div class="footer-link">
+                                <h3 class="footer-title">Categories</h3>
+                                <ul>
+                                    <li><a href="cart.html">My Order</a></li>
+                                    <li><a href="wishlist.html">Wishlist</a></li>
+                                    <li><a href="account.html">My Credit</a></li>
+                                    <li><a href="register.html">Login</a></li>
+                                    <li><a href="account.html">Personal Info</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 text-center">
+                        <p>&copy; Copyright Revel All Right Reserved</p>
+                    </div>
+                    <div class="col-12">
+                        <div class="part-img d-flex justify-content-center">
+                            <img src="assets/images/payment-gateway.png" alt="Image"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Footer
